5d235180b6f8a709639e316c562f5e74bd44fdd7
prukon <itmo158q1>
2021-11-25 01:24:36 +0300

    БД

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=prukon_rootime', 'prukon_rootime', 'Md&HY3Xt');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo->exec('SET NAMES "utf8"');
 
 
-$sql_details = array(
-'user' => 'prukon_rootime',
-'pass' => 'Md&HY3Xt',
-'db'   => 'prukon_gopet',
-'host' => 'localhost',
-'charset' => 'utf8'
-);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli("prukon.beget.tech", "prukon_rootime", "Md&HY3Xt", "prukon_rootime");
 
-//require( 'ssp.class.php' );
+
+//$result = $mysqli->query("SELECT * FROM users ORDER BY id ASC");
+// echo "<pre>";
+// print_r($result);
+// echo "</pre>";
