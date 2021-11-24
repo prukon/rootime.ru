c1e11a71fb681f0872ffe9433df3fa6f57020e48
prukon <itmo158q1>
2021-11-25 01:01:25 +0300

    test

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=prukon_gopet', 'prukon_gopet', 'Eld2TeBh');
+$pdo = new PDO('mysql:host=localhost;dbname=prukon_rootime', 'prukon_rootime', 'Md&HY3Xt');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec('SET NAMES "utf8"');
 
 
 $sql_details = array(
-'user' => 'prukon_gopet',
-'pass' => 'Eld2TeBh',
+'user' => 'prukon_rootime',
+'pass' => 'Md&HY3Xt',
 'db'   => 'prukon_gopet',
 'host' => 'localhost',
 'charset' => 'utf8'
 );
 
-require( 'ssp.class.php' );
+//require( 'ssp.class.php' );
