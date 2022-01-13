0d8683100450439a49791ed8e2c40491b782eb62
prukon <itmo158q1>
2022-01-14 00:02:21 +0300

    авторизация

--- index.php
+++ index.php
@@ -1,11 +1,58 @@
 <?php
+	/**
+	* Главный файл (переключатель)
+	* Site: http://bezramok-tlt.ru
+	* Регистрация пользователя письмом
+	*/
 
+	//Запускаем сессию
+	session_start();
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli("prukon.beget.tech", "prukon_rootime", "Md&HY3Xt", "prukon_rootime");
+	//Устанавливаем кодировку и вывод всех ошибок
+	header('Content-Type: text/html; charset=UTF8');
+	error_reporting(E_ALL);
 
+	//Включаем буферизацию содержимого
+	ob_start();
+ 
+	//Определяем переменную для переключателя
+	$mode = isset($_GET['mode'])  ? $_GET['mode'] : false;
+	$user = isset($_SESSION['user']) ? $_SESSION['user'] : false;
+	$err = array();
 
-//$result = $mysqli->query("SELECT * FROM users ORDER BY id ASC");
-// echo "<pre>";
-// print_r($result);
-// echo "</pre>";
+	//Устанавливаем ключ защиты
+	define('BEZ_KEY', true);
+	 
+	//Подключаем конфигурационный файл
+	include './config.php';
+	 
+	//Подключаем скрипт с функциями
+	include './func/funct.php';
+
+	//подключаем MySQL
+	include './bd/bd.php';
+
+	switch($mode)
+	{
+		//Подключаем обработчик с формой регистрации
+		case 'reg':
+			include './scripts/reg/reg.php';
+			include './scripts/reg/reg_form.html';
+		break;
+		
+		//Подключаем обработчик с формой авторизации
+		case 'auth':
+			include './scripts/auth/auth.php';
+			include './scripts/auth/auth_form.html';
+			include './scripts/auth/show.php';
+		break;
+    
+	}
+    
+	//Получаем данные с буфера
+	$content = ob_get_contents();
+	ob_end_clean();
+
+	//Подключаем наш шаблон
+	include './html/index.html';
+?>			
