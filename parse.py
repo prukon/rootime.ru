ad1119cf9a715fef757ee6d06bb8639c138d6304
prukon <itmo158q1>
2022-07-31 15:09:19 +0300

    parse

---
+++ parse.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+r = requests.get("https://stopgame.ru/review/new/izumitelno/p1")
+html = BS(r.content,'html.parser')
+
+for el in html.select(".items > .article-summary"):
+    title = el.select ('.caption > a')
+    print(title[0].text )
